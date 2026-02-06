00e05f57814223a39bddbe14b9148bf3c435c2e8
ElfCode67 <balmesgenrique27@gmail.com>
2026-02-06 22:22:50 +0800

    Basics

--- Fundamentals/main.py
+++ Fundamentals/main.py
@@ -1,13 +1,34 @@
-print("Hello World")
-print("It's really good")
-
+#print("Hello World")
+#print("It's really good") 
+# These are print statements 
 # This is a comment
 
-name = "Brada"
+#name = "Brada" 
+# This is a variable
 
-print(name)
+#print(name)
+#print(f"Hello {name}") 
 
+# Can use placeholders
+
+#food = "pizza"
+#print(f"You like {food}") 
+# More variables
+
+#type casting
+#gpa = 3.2
+#gpa = int(gpa)
+#print(gpa)
+# str(), int(), float(), bool()
+
+# input()
+
+name = input("What is your name? : ")
+age = input("What is your age?: ")
 print(f"Hello {name}")
+print(f"Hello {age}")
 
-food = "pizza"
-print(f"You like {food}")
+
+
+
+
