465a3a290307c506bd1f8f5fd9bb83ab35ef06e8
ElfCode67 <balmesgenrique27@gmail.com>
2026-02-06 22:32:45 +0800

    Madlibs game

---
+++ Madlibs game/main.py
@@ -0,0 +1,15 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb: ")
+adjective3 = input("Enter an adjective (description): ")
+verb2 = input("Enter a verb: ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"The {noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3} and decided to {verb2}!")
